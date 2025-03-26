0725a3d97966b6e6ddbcb29f87e73832fdf3104a
MyTPH <tan.phamhongmy@gmail.com>
2025-03-26 15:38:31 +0900

    Add files via upload

---
+++ baitap1.js
@@ -0,0 +1,10 @@
+let subject = [
+    "Math",
+    "Literature",
+    "Physics",
+    "Chemistry",
+    "History"
+]
+console.log(subject);
+subject.splice(2,0,"Art","Geography","Biology","Music","Civic Education");
+console.log(subject);
